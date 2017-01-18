af194297bb247887212af33009f222a2e9be8ac8
polang <grzx2210@163.com>
2017-01-18 14:13:08 +0800

    [modal] add modal-open on body

--- src/app/exports/modal/modal.component.ts
+++ src/app/exports/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, EventEmitter, Output, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, ViewChild, EventEmitter, Output, ChangeDetectionStrategy, Renderer } from '@angular/core';
 import { ModalOptions } from './modal-options.model';
 import { ModalWindowComponent } from './modal-window.component';
 
@@ -14,13 +14,19 @@ export class ModalComponent {
   @ViewChild(ModalWindowComponent) modalWindowComponent: ModalWindowComponent;
   instanceCount = 0;
 
+  constructor(private renderer: Renderer) {
+
+  }
+
   open() {
     this.isOpen = true;
     this.modalWindowComponent.isOpen = true;
+    this.renderer.setElementClass(document.body, 'modal-open', true);
   }
 
   close() {
     this.isOpen = false;
+    this.renderer.setElementClass(document.body, 'modal-open', false);
   }
 
   addContent<T>(options: ModalOptions, instanceCount: number): EventEmitter<T> {
