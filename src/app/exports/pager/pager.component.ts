d05c06353b0cd993c027edce5c096aa90c71c444
polang <grzx2210@163.com>
2017-01-19 14:05:16 +0800

    [pagination] optimize

--- src/app/exports/pager/pager.component.ts
+++ src/app/exports/pager/pager.component.ts
@@ -1,4 +1,7 @@
-import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import {
+  Component, ChangeDetectionStrategy, Input, Output, EventEmitter, OnChanges,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 're-pager',
@@ -6,6 +9,7 @@ import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter, OnChan
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PagerComponent implements OnChanges {
+  static EFFECT_PAGE_RANGE_KEYS = ['total', 'pageSize', 'pageIndex'];
   @Input() total = 0;
   @Input() pageSize = 10;
   @Input() pageIndex = 1;
@@ -14,6 +18,7 @@ export class PagerComponent implements OnChanges {
   @Input() previousText = '« Previous';
   @Input() nextText = 'Next »';
   @Input() disable = false;
+  totalPage = 0;
 
   prev(): void {
     if (this.hasPrev()) {
@@ -32,14 +37,19 @@ export class PagerComponent implements OnChanges {
   }
 
   hasNext(): boolean {
-    return this.pageIndex < this.totalPage();
+    return this.pageIndex < this.totalPage;
   }
 
-  totalPage(): number {
+  private getTotalPage() {
     return Math.ceil(this.total / this.pageSize);
   }
 
-  ngOnChanges(): void {
-    this.pageIndex = Math.max(Math.min(this.pageIndex, this.totalPage()), 1);
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const shouldUpdateRanges = PagerComponent.EFFECT_PAGE_RANGE_KEYS.some(key => !!changes[key]);
+    if (shouldUpdateRanges) {
+      this.totalPage = this.getTotalPage();
+      this.pageIndex = Math.max(Math.min(this.pageIndex, this.totalPage), 1);
+    }
   }
 }
