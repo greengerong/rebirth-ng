00f5f8a3e56f31d77039d641226ea18fbcc94bd2
polang <grzx2210@163.com>
2017-01-22 19:47:59 +0800

    [Rating] Disable Rating

--- src/app/demo/rating/rating-demo.component.ts
+++ src/app/demo/rating/rating-demo.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './rating-demo.component.html'
 })
 export class RatingDemoComponent implements OnInit {
+  disabled = false;
   rating: any = {
     value: 3,
     max: 10
