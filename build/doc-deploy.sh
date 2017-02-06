62138f399d2327365216655586d01cb79310483a
polang <grzx2210@163.com>
2017-02-06 17:43:29 +0800

    [Tech] document

--- build/doc-deploy.sh
+++ build/doc-deploy.sh
@@ -1,9 +1,9 @@
-npm run github:deploy
-npm run compodocs:deploy
+## npm run github:deploy
+## npm run compodocs:deploy
 git checkout gh-pages
 cp ./dist/compodocs ./
 git add ./compodocs/** --all
 git commit -m "rebirth ui compodocs docs deploy"
 git push
 echo 'compodocs docs deploy success...........'
-git checkout master
+## git checkout master
