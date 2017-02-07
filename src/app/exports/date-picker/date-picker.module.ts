5bb65afc416d065c901069231eb9de7f64b559fd
polang <grzx2210@163.com>
2017-02-07 14:50:14 +0800

    [DatePicker] date shim

--- src/app/exports/date-picker/date-picker.module.ts
+++ src/app/exports/date-picker/date-picker.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { DatePickerDirective } from './date-picker.directive';
 import { DatePickerPopupComponent } from './date-picker-popup.component';
 import { FormsModule } from '@angular/forms';
+
+import './date-parse.shim';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
