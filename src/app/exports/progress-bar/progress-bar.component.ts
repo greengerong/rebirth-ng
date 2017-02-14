25297ebea8a76ccb8496f404d06a9ddad008397e
polang <grzx2210@163.com>
2017-02-14 18:32:37 +0800

    [progress-bar] progress-bar

--- src/app/exports/progress-bar/progress-bar.component.ts
+++ src/app/exports/progress-bar/progress-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 
 @Component({
   selector: 're-progress-bar',
@@ -6,6 +6,13 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProgressBarComponent implements OnInit {
+  @Input() type = '';
+  @Input() text: string;
+  @Input() max = 100;
+  @Input() value: number = 45;
+  @Input() animate = false;
+  @Input() striped = false;
+
   constructor() {
   }
 
