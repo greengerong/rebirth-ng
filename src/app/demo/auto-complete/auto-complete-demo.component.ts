465d4748f7a4673cda997c810ccdfafa153bdd70
polang <grzx2210@163.com>
2017-02-26 01:01:34 +0800

    [AutoComplete] AutoComplete

--- src/app/demo/auto-complete/auto-complete-demo.component.ts
+++ src/app/demo/auto-complete/auto-complete-demo.component.ts
@@ -17,8 +17,11 @@ export class AutoCompleteDemoComponent implements OnInit {
   selectItem1: string;
   selectItem2: string;
   selectItem3: string;
+  selectItem4: string;
   isDisabled = false;
   icons = ['glyphicon-asterisk', 'glyphicon glyphicon-plus', 'glyphicon glyphicon-euro', 'glyphicon glyphicon-eu'];
+  languages = ['C#', 'C', 'C++', 'CPython', 'Java', 'JavaScript', 'Go', 'Python', 'Ruby', 'F#', 'TypeScript', 'SQL',
+    'LiveScript', 'CoffeeScript'];
 
   constructor(private jsonp: Jsonp) {
   }
@@ -30,10 +33,16 @@ export class AutoCompleteDemoComponent implements OnInit {
     return this.icons[index % this.icons.length];
   }
 
+
   onSearchLocal(term) {
-    return of(['C#', 'C', 'C++', 'CPython', 'Java', 'JavaScript', 'Go', 'Python', 'Ruby', 'F#', 'TypeScript', 'SQL',
-      'LiveScript', 'CoffeeScript']
-      .filter(lang => lang.toLowerCase().indexOf(term.toLowerCase()) !== -1));
+    return of(this.languages.filter(lang => lang.toLowerCase().indexOf(term.toLowerCase()) !== -1));
+  }
+
+  onSearchObject(term) {
+    return of(this.languages
+      .map((lang, index) => ({ label: lang, id: index }))
+      .filter(lang => lang.label.toLowerCase().indexOf(term.toLowerCase()) !== -1)
+    );
   }
 
   onSearch(term) {
