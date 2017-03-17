0191bf28ab8d2620d1e2907ccaeb9ee1e2876d2b
polang <grzx2210@163.com>
2017-03-17 14:02:39 +0800

    [DatePicker] DatePicker model

--- src/app/demo/date-picker/date-picker-demo.component.ts
+++ src/app/demo/date-picker/date-picker-demo.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 export class DatePickerDemoComponent implements OnInit {
   dateFormatOptions = ['YYYY-MM-DD HH:mm', 'YY-MM-DD HH:mm', 'MM/DD/YYYY HH:mm', 'MM/DD/YY HH:mm'];
   dateFormat: string;
-  selectedDate1: Date;
+  selectedDate1 = new Date('01/02/2017 09:11');
   selectedDate2: Date;
   selectedDate3 = new Date('01/02/2017 09:11');
 
