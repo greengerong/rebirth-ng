a754fbb6317a904c373849175310ce609413606b
polang <grzx2210@163.com>
2017-03-22 00:25:35 +0800

    [publish] npm publish

--- src/app/exports/common/router-reuse-strategy.ts
+++ src/app/exports/common/router-reuse-strategy.ts
@@ -5,7 +5,7 @@ export class RebirthRouterReuseStrategy implements RouteReuseStrategy {
   handlers: { [key: string]: DetachedRouteHandle } = {};
 
   shouldDetach(route: ActivatedRouteSnapshot): boolean {
-    return true;
+    return this.isReusable(route);
   }
 
   store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
@@ -17,11 +17,15 @@ export class RebirthRouterReuseStrategy implements RouteReuseStrategy {
   }
 
   retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
-    return route.routeConfig ? this.handlers[route.routeConfig.path] : null;
+    return this.isReusable(route) && route.routeConfig ? this.handlers[route.routeConfig.path] : null;
   }
 
   shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
-    return future.routeConfig === curr.routeConfig;
+    return future.routeConfig === curr.routeConfig && this.isReusable(future);
+  }
+
+  private isReusable(route: ActivatedRouteSnapshot) {
+    return route.data && route.data['reusable'];
   }
 
 }
--- src/app/exports/modal/README.md
+++ src/app/exports/modal/README.md
@@ -1,6 +1,17 @@
 
 #### Notices
  
+* Shoulde pass `rebirthUIConfig.rootContainer` in your `AppComponent` when you did not pass `rootContainer` parameters to `ModalService`.
+
+
+    export class AppComponent implements OnInit {
+      constructor(private rebirthConfig: RebirthUIConfig,
+                  private viewContainerRef: ViewContainerRef) {
+                  
+        this.rebirthConfig.rootContainer = this.viewContainerRef; // default container for append body component(Modal, DatePicker...)
+        // this.rebirthConfig.extend(REBIRTH_UI_I18N_ZHCN); i18n
+      }
+
 * Make sure put your modal component to `entryComponents` of your module! Example with `ModalTestComponent`: 
 
     
