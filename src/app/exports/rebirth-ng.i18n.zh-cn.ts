28b56c272a0d5b694c4a9f29bf7f130f2a04e7c4
polang <grzx2210@163.com>
2017-03-24 20:00:41 +0800

    [rename] rename project to rebirth/ng

---
+++ src/app/exports/rebirth-ng.i18n.zh-cn.ts
@@ -0,0 +1,53 @@
+export const REBIRTH_NG_I18N_ZHCN = {
+
+  datePicker: {
+    locale: 'zh-CN',
+    today: '今天',
+    timePicker: false,
+    dateConverter: null, // DateConverter
+    weeks: ['日', '一', '二', '三', '四', '五', '六'],
+    months: ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二'],
+    min: 1900,
+    max: 2099,
+    format: {
+      date: 'YYYY-MM-DD',
+      time: 'YYYY-MM-DD HH:mm'
+    }
+  },
+
+  dialog: {
+    button: {
+      yes: '确定',
+      no: '取消'
+    }
+  },
+
+  pager: {
+    pageSize: 10,
+    aligned: true,
+    button: {
+      previous: '« 上一页',
+      next: '下一页 »'
+    }
+  },
+
+  pagination: {
+    boundary: true,
+    pageSize: 10,
+    maxItems: 10,
+    size: '', // '' | 'lg' | 'sm'
+    button: {
+      first: '首页',
+      last: '尾页',
+      pre: '前一页',
+      next: '下一页'
+    }
+  },
+
+  switchBtn: {
+    onText: '开启',
+    offText: '关闭',
+    type: 'primary'
+  }
+
+};
---
+++ src/app/exports/rebirth-ng.module.ts
@@ -0,0 +1,111 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { ModalService, ModalModule } from './modal';
+import { DialogService, DialogModule } from './dialog';
+import { PagerModule } from './pager';
+import { RebirthNGConfig } from './rebirth-ng.config';
+import { PaginationModule } from './pagination';
+import { COMMON_SERVICES } from './window-ref';
+import { AlertBoxModule } from './alert-box';
+import { BadgeModule } from './badge';
+import { PanelModule } from './panel';
+import { AccordionModule } from './accordion';
+import { ActionButtonModule } from './action-button';
+import { RatingModule } from './rating';
+import { TabsModule } from './tabs';
+import { TooltipModule } from './tooltip';
+import { PositionService } from './position';
+import { PopoverModule } from './popover';
+import { DatePickerModule } from './date-picker';
+import { BreadcrumbsModule } from './breadcrumbs';
+import { ProgressBarModule } from './progress-bar';
+import { SelectButtonModule } from './select-button';
+import { OverlayModule, OverlayService } from './overlay';
+import { CarouselModule } from './carousel';
+import { SwitchModule } from './switch/switch.module';
+import { MenuBarModule } from './menu-bar/menu-bar.module';
+import { AutoCompleteModule } from './auto-complete';
+import { FlowStepModule } from './flow-step';
+import { DataTableModule } from './data-table';
+import { InfiniteScrollModule } from './infinite-scroll';
+import { RebirthCommonModule } from './common';
+import { DraggableModule } from './draggable';
+
+
+@NgModule({
+  imports: [
+    RebirthCommonModule,
+    ModalModule,
+    DialogModule,
+    PagerModule,
+    PaginationModule,
+    AlertBoxModule,
+    BadgeModule,
+    PanelModule,
+    AccordionModule,
+    ActionButtonModule,
+    RatingModule,
+    TabsModule,
+    TooltipModule,
+    PopoverModule,
+    DatePickerModule,
+    BreadcrumbsModule,
+    ProgressBarModule,
+    SelectButtonModule,
+    OverlayModule,
+    CarouselModule,
+    SwitchModule,
+    MenuBarModule,
+    AutoCompleteModule,
+    FlowStepModule,
+    InfiniteScrollModule,
+    DataTableModule,
+  ],
+  exports: [
+    RebirthCommonModule,
+    ModalModule,
+    DialogModule,
+    PagerModule,
+    PaginationModule,
+    AlertBoxModule,
+    BadgeModule,
+    PanelModule,
+    AccordionModule,
+    ActionButtonModule,
+    RatingModule,
+    TabsModule,
+    TooltipModule,
+    PopoverModule,
+    DatePickerModule,
+    BreadcrumbsModule,
+    ProgressBarModule,
+    SelectButtonModule,
+    OverlayModule,
+    CarouselModule,
+    SwitchModule,
+    MenuBarModule,
+    AutoCompleteModule,
+    FlowStepModule,
+    InfiniteScrollModule,
+    DataTableModule,
+    DraggableModule
+  ],
+  declarations: [],
+  providers: [],
+})
+export class RebirthNGModule {
+
+  static forRoot(): ModuleWithProviders {
+
+    return {
+      ngModule: RebirthNGModule,
+      providers: [
+        ...COMMON_SERVICES,
+        { provide: RebirthNGConfig, useClass: RebirthNGConfig },
+        { provide: ModalService, useClass: ModalService },
+        { provide: DialogService, useClass: DialogService },
+        { provide: PositionService, useClass: PositionService },
+        { provide: OverlayService, useClass: OverlayService }
+      ]
+    };
+  }
+}
