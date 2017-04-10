21fb56dfd0fc4dff165c85e6bd86a1d2d088a8cf
polang <grzx2210@163.com>
2017-04-10 09:38:14 +0800

    [panel] remove without**

--- src/app/exports/panel/panel.component.ts
+++ src/app/exports/panel/panel.component.ts
@@ -1,7 +1,10 @@
-import { Component, Input, Output, EventEmitter, Optional, OnInit, Host, OnDestroy } from '@angular/core';
+import { Component, Input, Output, EventEmitter, Optional, OnInit, Host, OnDestroy, ContentChild } from '@angular/core';
 import { PanelGroup } from './panel-group.model';
 import { RebirthNGConfig } from '../rebirth-ng.config';
 import { stopPropagationIfExist } from '../utils/dom-utils';
+import { PanelHeaderComponent } from './panel-header.component';
+import { PanelBodyComponent } from './panel-body.component';
+import { PanelFooterComponent } from './panel-footer.component';
 
 @Component({
   selector: 're-panel,re-accordion-item',
@@ -12,13 +15,14 @@ export class PanelComponent implements OnInit, OnDestroy {
   @Input() id;
   @Input() type: 'default' | 'primary' | 'success' | 'info' | 'warning' | 'danger' = 'default';
   @Input() closable: boolean;
-  @Input() withoutHeader: boolean;
-  @Input() withoutFooter = true;
   @Input() collapsable: boolean;
   @Input() isCollapsed = false;
   @Input() cssClass: string;
   @Output() close = new EventEmitter<PanelComponent>();
   @Output() collapse = new EventEmitter<boolean>();
+  @ContentChild(PanelHeaderComponent) panelHeader;
+  @ContentChild(PanelBodyComponent) panelBody;
+  @ContentChild(PanelFooterComponent) panelFooter;
 
   constructor(@Optional() @Host() private  panelGroup: PanelGroup, private rebirthUIConfig: RebirthNGConfig) {
     this.type = <any>rebirthUIConfig.panel.type;
