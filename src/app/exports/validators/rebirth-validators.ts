a0f065c83a19845929e5bef89cb5dca978cd2271
polang <grzx2210@163.com>
2017-05-09 00:02:14 +0800

    [RebirthValidators]  RebirthValidators

--- src/app/exports/validators/rebirth-validators.ts
+++ src/app/exports/validators/rebirth-validators.ts
@@ -1,4 +1,4 @@
-import { ValidatorFn, AbstractControl, Validators, FormGroup } from '@angular/forms';
+import { ValidatorFn, AbstractControl, Validators } from '@angular/forms';
 
 export function isPresent(obj: any): boolean {
   return obj !== undefined && obj !== null;
@@ -56,122 +56,140 @@ export class RebirthValidators {
   /**
    * Validator that requires controls to have a value of digits.
    */
-  static digits(control: AbstractControl): { [key: string]: boolean } {
-    if (isPresent(Validators.required(control))) return null;
+  static digits(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (isPresent(Validators.required(control))) return null;
 
-    let v: string = control.value;
-    return /^\d+$/.test(v) ? null : { 'digits': true };
+      let v: string = control.value;
+      return /^\d+$/.test(v) ? null : { 'digits': true };
+    };
   }
 
   /**
    * Validator that requires controls to have a value of number.
    */
-  static number(control: AbstractControl): { [key: string]: boolean } {
-    if (isPresent(Validators.required(control))) return null;
+  static number(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (isPresent(Validators.required(control))) return null;
 
-    let v: string = control.value;
-    return /^(?:-?\d+|-?\d{1,3}(?:,\d{3})+)?(?:\.\d+)?$/.test(v) ? null : { 'number': true };
+      let v: string = control.value;
+      return /^(?:-?\d+|-?\d{1,3}(?:,\d{3})+)?(?:\.\d+)?$/.test(v) ? null : { 'number': true };
+    };
   }
 
   /**
    * Validator that requires controls to have a value of url.
    */
-  static url(control: AbstractControl): { [key: string]: boolean } {
-    if (isPresent(Validators.required(control))) return null;
+  static url(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (isPresent(Validators.required(control))) return null;
 
-    let v: string = control.value;
-    return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(v) ? null : { 'url': true };
+      let v: string = control.value;
+      return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(v) ? null : { 'url': true };
+    };
+  }
+
+  static email(control: AbstractControl): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (isPresent(Validators.required(control))) return null;
+      let v: string = control.value;
+      return /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(v) ? null : { 'email': true };
+    };
   }
 
   /**
    * Validator that requires controls to have a value of date.
    */
-  static date(control: AbstractControl): { [key: string]: boolean } {
-    if (isPresent(Validators.required(control))) return null;
+  static date(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (isPresent(Validators.required(control))) return null;
 
-    let v: string = control.value;
-    return !/Invalid|NaN/.test(new Date(v).toString()) ? null : { 'date': true };
+      let v: string = control.value;
+      return !/Invalid|NaN/.test(new Date(v).toString()) ? null : { 'date': true };
+    };
   }
 
   /**
    * Validator that requires controls to have a value of dateISO.
    */
-  static dateISO(control: AbstractControl): { [key: string]: boolean } {
-    if (isPresent(Validators.required(control))) return null;
+  static dateISO(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (isPresent(Validators.required(control))) return null;
 
-    let v: string = control.value;
-    return /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(v) ? null : { 'dateISO': true };
+      let v: string = control.value;
+      return /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(v) ? null : { 'dateISO': true };
+    };
   }
 
   /**
    * Validator that requires controls to have a value of creditCard.
    */
-  static creditCard(control: AbstractControl): { [key: string]: boolean } {
-    if (isPresent(Validators.required(control))) return null;
+  static creditCard(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (isPresent(Validators.required(control))) return null;
+      let v: string = control.value;
+      let sanitized = v.replace(/[^0-9]+/g, '');
 
-    let v: string = control.value;
+      // problem with chrome
+      if (!(/^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/.test(sanitized))) {
+        return { 'creditCard': true };
+      }
 
-    let sanitized = v.replace(/[^0-9]+/g, '');
-
-    // problem with chrome
-    if (!(/^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/.test(sanitized))) {
-      return { 'creditCard': true };
-    }
-
-    let sum = 0;
-    let digit;
-    let tmpNum;
-    let shouldDouble;
-    for (let i = sanitized.length - 1; i >= 0; i--) {
-      digit = sanitized.substring(i, (i + 1));
-      tmpNum = parseInt(digit, 10);
-      if (shouldDouble) {
-        tmpNum *= 2;
-        if (tmpNum >= 10) {
-          sum += ((tmpNum % 10) + 1);
+      let sum = 0;
+      let digit;
+      let tmpNum;
+      let shouldDouble;
+      for (let i = sanitized.length - 1; i >= 0; i--) {
+        digit = sanitized.substring(i, (i + 1));
+        tmpNum = parseInt(digit, 10);
+        if (shouldDouble) {
+          tmpNum *= 2;
+          if (tmpNum >= 10) {
+            sum += ((tmpNum % 10) + 1);
+          } else {
+            sum += tmpNum;
+          }
         } else {
           sum += tmpNum;
         }
-      } else {
-        sum += tmpNum;
+        shouldDouble = !shouldDouble;
       }
-      shouldDouble = !shouldDouble;
-    }
 
-    if (Boolean((sum % 10) === 0 ? sanitized : false)) {
-      return null;
-    }
+      if (Boolean((sum % 10) === 0 ? sanitized : false)) {
+        return null;
+      }
 
-    return { 'creditCard': true };
+      return { 'creditCard': true };
+    };
   }
 
   /**
    * Validator that requires controls to have a value of JSON.
    */
-  static json(control: AbstractControl): { [key: string]: boolean } {
-    if (isPresent(Validators.required(control))) return null;
-
-    let v: string = control.value;
-
-    try {
-      let obj = JSON.parse(v);
-
-      if (Boolean(obj) && typeof obj === 'object') {
-        return null;
+  static json(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (isPresent(Validators.required(control))) return null;
+      let v: string = control.value;
+      try {
+        let obj = JSON.parse(v);
+        if (Boolean(obj) && typeof obj === 'object') {
+          return null;
+        }
+      } catch (e) {
       }
-    } catch (e) {
-    }
-    return { 'json': true };
+      return { 'json': true };
+    };
   }
 
   /**
    * Validator that requires controls to have a value of base64.
    */
-  static base64(control: AbstractControl): { [key: string]: boolean } {
-    if (isPresent(Validators.required(control))) return null;
-
-    let v: string = control.value;
-    return /^(?:[A-Z0-9+\/]{4})*(?:[A-Z0-9+\/]{2}==|[A-Z0-9+\/]{3}=|[A-Z0-9+\/]{4})$/i.test(v) ? null : { 'base64': true };
+  static base64(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (isPresent(Validators.required(control))) return null;
+      let v: string = control.value;
+      return /^(?:[A-Z0-9+\/]{4})*(?:[A-Z0-9+\/]{2}==|[A-Z0-9+\/]{3}=|[A-Z0-9+\/]{4})$/i.test(v) ? null : { 'base64': true };
+    };
   }
 
   /**
@@ -247,22 +265,7 @@ export class RebirthValidators {
   /**
    * Validator that requires controls to have a value to equal another control.
    */
-  static equalTo(group: AbstractControl): { [key: string]: any } {
-    if (!(group instanceof FormGroup)) return null;
-    let fb = group as FormGroup;
-    let keys: string[] = Object.keys(fb.controls);
-    let len: number = keys.length;
-
-    if (!len) return null;
-
-    let firstKey = keys[0];
-
-    for (let i = 1; i < len; i++) {
-      if (fb.controls[firstKey].value !== fb.controls[keys[i]].value) {
-        return { equalTo: true };
-      }
-    }
-
+  static equalTo(): ValidatorFn {
     return null;
   }
 }
