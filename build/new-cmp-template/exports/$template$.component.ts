745ed60ae6626fe908132aede28b33974d85e31e
polang <grzx2210@163.com>
2017-05-08 23:18:22 +0800

    [Template]  Template

--- build/new-cmp-template/exports/$template$.component.ts
+++ build/new-cmp-template/exports/$template$.component.ts
@@ -3,6 +3,7 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 're-<%= componentSelector %>',
   templateUrl: './<%= componentSelector %>.component.html',
+  styleUrls: ['./<%= componentSelector %>.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   exportAs: '<%= componentName %>'
 })
