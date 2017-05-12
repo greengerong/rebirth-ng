81f28ab677166552ac9f29e065a13a3a36aa7aeb
Bang Wu <ohwubang@gmail.com>
2017-05-12 09:31:34 +0800

    [test] add the code coverage and tix the test

--- src/app/exports/file-upload/file-upload.component.spec.ts
+++ src/app/exports/file-upload/file-upload.component.spec.ts
@@ -2,12 +2,32 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { FileUploadComponent } from './file-upload.component';
+import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
+import { AlertBoxModule } from '../alert-box/alert-box.module';
+import { DraggableModule } from '../draggable/draggable.module';
+import { FileUploadPreviewComponent } from './file-upload-preview.component';
+import {
+  UploadFileTmplComponent, UploadPreviewTmplComponent,
+  UploadToolbarTmplComponent
+} from './file-upload-tmpl.directive';
 
 describe('FileUpload', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        HttpModule,
+        AlertBoxModule,
+        DraggableModule
+      ],
+      providers: [],
       declarations: [
-        FileUploadComponent
+        FileUploadComponent,
+        FileUploadPreviewComponent,
+        UploadToolbarTmplComponent,
+        UploadPreviewTmplComponent,
+        UploadFileTmplComponent
       ],
     });
     TestBed.compileComponents();
