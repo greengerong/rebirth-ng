c85ea5a8973886d72ba309bef3707fb4cea3b6e4
wfsovereign <wfsovereign@outlook.com>
2017-05-13 18:57:13 +0800

    [time-picker] use different model

--- src/app/demo/time-picker/time-picker-demo.component.ts
+++ src/app/demo/time-picker/time-picker-demo.component.ts
@@ -7,7 +7,9 @@ import { TimePickerModel } from '../../exports';
 })
 export class TimePickerDemoComponent {
 
-  time = new TimePickerModel(13, 30);
+  time1 = new TimePickerModel(13, 30, 30);
+  time2 = new TimePickerModel(13, 30);
+  time3 = new TimePickerModel(13, 30, 30);
   disabled: boolean;
 
   timeChange(time) {
