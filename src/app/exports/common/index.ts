4a8c28b9bae12211b871dd7545fe733d75c045a9
polang <grzx2210@163.com>
2017-05-16 21:49:14 +0800

    [DataTable] columns change

--- src/app/exports/common/index.ts
+++ src/app/exports/common/index.ts
@@ -1,6 +1,7 @@
 export * from './auto-focus.directive';
 export * from './router-reuse-strategy';
 export * from './assets-loader.service';
+export * from './resizeable.directive';
 export * from './template-loader.component';
 export * from './trust-html.pipe';
 export * from './common.module';
