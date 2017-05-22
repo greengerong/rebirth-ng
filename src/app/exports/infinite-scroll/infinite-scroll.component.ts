4370afcd0a13934a2997abe987d98599346ea91a
polang <grzx2210@163.com>
2017-05-22 14:11:28 +0800

    [infiniteScroll] infiniteScroll load when first time

--- src/app/exports/infinite-scroll/infinite-scroll.component.ts
+++ src/app/exports/infinite-scroll/infinite-scroll.component.ts
@@ -36,6 +36,7 @@ export class InfiniteScrollComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.setupScrollEvent();
+    setTimeout(_ => this.onWinScroll(null));
   }
 
   loadFinish(complete: boolean) {
