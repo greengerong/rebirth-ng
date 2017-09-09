f8d219915bfe5e5f0832707273b0259a6471ae9c
polang <grzx2210@163.com>
2017-09-09 16:57:28 +0800

    [build] coverage excludes & limit

--- karma.conf.js
+++ karma.conf.js
@@ -9,30 +9,49 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-istanbul-threshold'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     files: [
-      { pattern: './src/test.ts', watched: false }
+      {pattern: './src/test.ts', watched: false}
     ],
     preprocessors: {
       './src/test.ts': ['@angular/cli']
     },
     mime: {
-      'text/x-typescript': ['ts','tsx']
+      'text/x-typescript': ['ts', 'tsx']
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: ['html', 'json'],
       fixWebpackSourcePaths: true
+    },
+    istanbulThresholdReporter: {
+      src: 'coverage/coverage-final.json',
+      reporters: ['text'],
+      thresholds: {
+        global: {
+          statements: 0,
+          branches: 0,
+          lines: 0,
+          functions: 0,
+        },
+        each: {
+          statements: 0,
+          branches: 0,
+          lines: 0,
+          functions: 0,
+        },
+      }
     },
     angularCli: {
       environment: 'dev'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
-      ? ['progress', 'coverage-istanbul']
+      ? ['progress', 'coverage-istanbul', 'istanbul-threshold']
       : ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
