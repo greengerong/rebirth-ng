c09621e54f9909b457b657c55ad5da638e56ebab
polang <grzx2210@163.com>
2017-10-14 22:24:31 +0800

    [ng5] replace httpmodule to httpclientmodule

--- src/app/exports/file-upload/file-upload.component.spec.ts
+++ src/app/exports/file-upload/file-upload.component.spec.ts
@@ -3,7 +3,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { FileUploadComponent } from './file-upload.component';
 import { CommonModule } from '@angular/common';
-import { HttpModule } from '@angular/http';
 import { AlertBoxModule } from '../alert-box/alert-box.module';
 import { DraggableModule } from '../draggable/draggable.module';
 import { FileUploadPreviewComponent } from './file-upload-preview.component';
@@ -11,13 +10,14 @@ import {
   UploadFileTmplComponent, UploadPreviewTmplComponent,
   UploadToolbarTmplComponent
 } from './file-upload-tmpl.directive';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('FileUpload', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         CommonModule,
-        HttpModule,
+        HttpClientModule,
         AlertBoxModule,
         DraggableModule
       ],
